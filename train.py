0d5ff96b5d533a8bb6f47f36858b94ff1ef7ff7c
Abrar <abrar9597@gmail.com>
2020-11-17 13:49:36 +0600

    freezing layers 1 to 10

--- train.py
+++ train.py
@@ -81,6 +81,8 @@ def train(hyp, opt, device, tb_writer=None):
 
     # Freeze
     freeze = ['', ]  # parameter names to freeze (full or partial)
+
+    freeze = ['model.%s' % x for x in range(10)]
     if any(freeze):
         for k, v in model.named_parameters():
             if any(x in k for x in freeze):
